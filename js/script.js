ac1b7c8b28a9007ea0a09eac4c83d0ac90cfafcb
Marcin <cin.miko94@gmail.com>
2020-05-06 22:43:55 +0200

    Submodule 5.3

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let computerMove = `kamień`;
+let playerMove = `papier`;
+//  To o to chodziło ?? (Submoduł 5.3)
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
