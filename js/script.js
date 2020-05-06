7355e52c3e76f8d8724051dce60daafba1fe6902
Marcin <cin.miko94@gmail.com>
2020-05-06 23:08:06 +0200

    Reading the Computer and the Player movements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,42 @@
 let computerMove = `kamień`;
 let playerMove = `papier`;
-//  To o to chodziło ?? (Submoduł 5.3)
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+switch(playerInput) {
+    case 1: {
+        playerMove = `kamień`;
+        break;
+    }
+    case 2: {
+        playerMove = `papier`;
+        break;
+    }
+    case 3: {
+        playerMove = `nożyce`;
+        break;
+    }
+    default: {
+        playerMove = `Nieznany Ruch`;
+        break;
+    }
+}
+
+switch(randomNumber) {
+    case 1: {
+        computerMove = `kamień`;
+        break;
+    }
+    case 2: {
+        computerMove = `papier`;
+        break;
+    }
+    case 3: {
+        computerMove = `nożyce`;
+        break;
+    }
+    default: {
+        computerMove = `Nieznany Ruch`;
+        break;
+    }
+}
